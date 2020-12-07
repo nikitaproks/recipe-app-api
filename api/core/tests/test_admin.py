a9646801be0e15599d1e48cc7581e7a30550fa2c
nikita <prokayev@icloud.com>
2020-12-07 21:03:00 +0200

    changing travis ci file

--- api/core/tests/test_admin.py
+++ api/core/tests/test_admin.py
@@ -1,4 +1,4 @@
-""" from django.test import TestCase
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
@@ -20,10 +20,9 @@ class AdminSiteTests(TestCase):
         )
 
     def test_users_listed(self):
-        Test that users are listed on the user page
+        """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
- """
